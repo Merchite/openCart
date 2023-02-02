312f02cd69a1464580667cfce02ec6d7991e6f3b
ben-mtg <118884258+ben-mtg@users.noreply.github.com>
2023-02-02 16:54:11 +0100

    Added plugin Pay.nl

---
+++ upload/admin/controller/extension/payment/paynl_klarnakp.php
@@ -0,0 +1,13 @@
+<?php
+$dir = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
+$autoload = $dir . '/Pay/Autoload.php';
+
+require_once $autoload;
+
+class ControllerExtensionPaymentPaynlKlarnakp extends Pay_Controller_Admin
+{
+    protected $_paymentOptionId = 1717;
+    protected $_paymentMethodName = 'paynl_klarnakp';
+    protected $_postPayment = true;
+    protected $_defaultLabel = 'Klarna (Achteraf betalen)';
+}
