3ee53026b1134d2cec4d4297d6a8caa0139a51b1
ben-mtg <118884258+ben-mtg@users.noreply.github.com>
2023-02-03 08:17:31 +0100

    Added Pay.nl plugin

---
+++ upload/admin/language/en-gb/extension/payment/paynl_capayablegespreid.php
@@ -0,0 +1,4 @@
+<?php
+include 'paynl3.php';
+// Heading
+$_['heading_title'] = 'PAY. - in3 keer betalen, 0% rente';
---
+++ upload/admin/language/nl-nl/extension/payment/paynl_tikkie.php
@@ -0,0 +1,5 @@
+<?php
+include 'paynl3.php';
+// Heading
+$_['heading_title'] = 'PAY. - Tikkie';
+$_['text_extension'] = 'PAY.';
